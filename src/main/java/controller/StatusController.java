07af82a0a9c72bff0357076ac911842e62cdafa8
Paulo Roberto <evans_sp@hotmail.com>
2022-06-25 17:47:21 -0300

    Add classe controler e método para checar o status da api

---
+++ src/main/java/controller/StatusController.java
@@ -0,0 +1,15 @@
+package controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatusController {
+
+    //Criando o método para checar o status da api
+    @GetMapping(path = "/api/status")
+    public String check(){
+        return "online";
+    }
+
+}
+
