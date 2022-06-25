3577eb5cd7cb57468c2e15080859bed40a1d39de
Paulo Roberto <evans_sp@hotmail.com>
2022-06-25 17:38:31 -0300

    add spring no projeto

---
+++ src/main/java/estudos/java/api/rest/spring/ApiRestSpringApplication.java
@@ -0,0 +1,13 @@
+package estudos.java.api.rest.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/estudos/java/api/rest/spring/ApiRestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package estudos.java.api.rest.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
