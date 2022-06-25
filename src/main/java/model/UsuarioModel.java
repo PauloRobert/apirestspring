7c772175a3958c314459eba8e509a6e7d9b07a5b
Paulo Roberto <evans_sp@hotmail.com>
2022-06-25 18:02:57 -0300

    add package model e classe usuario

---
+++ src/main/java/model/UsuarioModel.java
@@ -0,0 +1,45 @@
+package model;
+
+import javax.persistence.Entity;
+
+//Classe de entidade de banco de dados
+@Entity (name = "usuario")
+public class UsuarioModel {
+
+    public Integer codigo;
+    public String nome;
+    public  String login;
+    public  String senha;
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
