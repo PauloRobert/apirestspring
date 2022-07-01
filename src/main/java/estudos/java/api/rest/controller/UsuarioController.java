45304f8bc8ce7cd90a459527b8892ca22c33c22d
Paulo Roberto <evans_sp@hotmail.com>
2022-07-01 18:22:02 -0300

    Add @ResponseStatus(HttpStatus.OK)

--- src/main/java/estudos/java/api/rest/controller/UsuarioController.java
+++ src/main/java/estudos/java/api/rest/controller/UsuarioController.java
@@ -3,6 +3,7 @@ package estudos.java.api.rest.controller;
 import estudos.java.api.rest.model.UsuarioModel;
 import estudos.java.api.rest.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 @RestController
@@ -12,6 +13,7 @@ public class UsuarioController {
     private UsuarioRepository repository;
 
     @GetMapping(path = "/api/usuario/{codigo}")
+    @ResponseStatus(HttpStatus.OK)
     public ResponseEntity consultar(@PathVariable("codigo") Integer codigo) {
         return repository.findById(codigo)
                 .map(record -> ResponseEntity.ok().body(record))
@@ -19,6 +21,7 @@ public class UsuarioController {
     }
 
     @PostMapping(path = "/api/usuario/salvar")
+    @ResponseStatus(HttpStatus.OK)
     public UsuarioModel salvar(@RequestBody UsuarioModel usuario) {
         return repository.save(usuario);
     }
