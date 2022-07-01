966b0741070b9cb6606e0378f78ab6149aeaccfe
Paulo Roberto <evans_sp@hotmail.com>
2022-07-01 17:25:49 -0300

    MOvendo a classe principal para fora do pacote

--- src/main/java/estudos/java/api/rest/ApiRestSpringApplication.java
+++ src/main/java/estudos/java/api/rest/ApiRestSpringApplication.java
@@ -1,4 +1,4 @@
-package estudos.java.api.rest.Api;
+package estudos.java.api.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
