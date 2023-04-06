73e6771812d7a08d8d6195074008ee0aa388d730
Paulo Roberto <evans_sp@hotmail.com>
2023-04-06 19:43:04 -0300

    Refatoração da classe por completo, reduzindo código e melhorando a legibilidade

--- src/main/java/estudos/java/api/rest/controller/UsuarioController.java
+++ src/main/java/estudos/java/api/rest/controller/UsuarioController.java
@@ -7,44 +7,66 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/usuario")
 public class UsuarioController {
 
     @Autowired
     private UsuarioRepository repository;
 
-    @GetMapping(path = "/api/usuario/{codigo}")
-    @ResponseStatus(HttpStatus.OK)
+    /**
+     * Consulta um usuário pelo código
+     *
+     * @param codigo o código do usuário a ser consultado
+     * @return o usuário encontrado ou uma mensagem de erro
+     */
+    @GetMapping(path = "/{codigo}")
     public ResponseEntity consultar(@PathVariable("codigo") Integer codigo) {
-        return repository.findById(codigo)
-                .map(record -> ResponseEntity.ok().body(record))
-                .orElse(ResponseEntity.notFound().build());
-    }
-
-    @GetMapping(path = "/api/usuario/listar")
-    @ResponseBody
-    public ResponseEntity<List<UsuarioModel>> listaUsuario(){
-        List<UsuarioModel> usuarios = (List<UsuarioModel>) repository.findAll();
-
-        return new ResponseEntity<List<UsuarioModel>>(usuarios, HttpStatus.OK);
-    }
-
-    @PostMapping(path = "/api/usuario/salvar")
-    @ResponseStatus(HttpStatus.OK)
-    public UsuarioModel salvar(@RequestBody UsuarioModel usuario) {
-        try {
-            return repository.save(usuario);
-        } catch (Exception msg) {
-            System.out.println("Erro interno");
+        Optional<UsuarioModel> usuarioOpt = repository.findById(codigo);
+        if (usuarioOpt.isPresent()) {
+            return ResponseEntity.ok().body(usuarioOpt.get());
+        } else {
+            return ResponseEntity.notFound().build();
         }
-        return usuario;
     }
 
-    @DeleteMapping(path = "/api/usuario/{codigo}")
+    /**
+     * Lista todos os usuários cadastrados
+     *
+     * @return a lista de usuários ou uma mensagem de erro
+     */
+    @GetMapping(path = "/listar")
+    public ResponseEntity<List<UsuarioModel>> listar() {
+        List<UsuarioModel> usuarios = repository.findAll();
+        return ResponseEntity.ok().body(usuarios);
+    }
+
+    /**
+     * Salva um usuário
+     *
+     * @param usuario o usuário a ser salvo
+     * @return o usuário salvo ou uma mensagem de erro
+     */
+    @PostMapping(path = "/salvar")
+    public ResponseEntity<UsuarioModel> salvar(@RequestBody UsuarioModel usuario) {
+        try {
+            UsuarioModel usuarioSalvo = repository.save(usuario);
+            return ResponseEntity.ok().body(usuarioSalvo);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    /**
+     * Deleta um usuário pelo código
+     *
+     * @param codigo o código do usuário a ser deletado
+     * @return uma mensagem de sucesso ou de erro
+     */
+    @DeleteMapping(path = "/{codigo}")
     public ResponseEntity<String> deletar(@PathVariable("codigo") Integer codigo) {
         Optional<UsuarioModel> usuarioOpt = repository.findById(codigo);
         if (usuarioOpt.isPresent()) {
@@ -53,7 +75,7 @@ public class UsuarioController {
             repository.delete(usuario);
             return ResponseEntity.ok("Usuário " + nomeUsuario + " deletado com sucesso!");
         } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado, já deletado ou nunca cadastrado!");        }
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado, já deletado ou nunca cadastrado!");
+        }
     }
-
 }
