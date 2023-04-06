090760ac04e2b76d3ff844eb560bb9c8fc3cafe7
Paulo Roberto <evans_sp@hotmail.com>
2023-04-06 19:35:28 -0300

    Incluindo o endpoint de delete de usuário!

--- src/main/java/estudos/java/api/rest/controller/UsuarioController.java
+++ src/main/java/estudos/java/api/rest/controller/UsuarioController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.websocket.server.PathParam;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UsuarioController {
