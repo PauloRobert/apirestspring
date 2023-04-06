bfb4bf0fcdf01c1fec776406a62cf1b76ee922c8
Paulo Roberto <evans_sp@hotmail.com>
2023-04-06 20:19:09 -0300

    Concluido a refatoração!

--- src/main/java/estudos/java/api/rest/ApiRestSpringApplication.java
+++ src/main/java/estudos/java/api/rest/ApiRestSpringApplication.java
@@ -1,13 +1,12 @@
 package estudos.java.api.rest;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class ApiRestSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiRestSpringApplication.class, args);
 	}
-
 }
