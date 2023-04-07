7c08b41d769510a9a29a287e34c005bab4ce76d1
Paulo Roberto <evans_sp@hotmail.com>
2023-04-06 21:07:48 -0300

    Utilizando o RepeatedTest

--- src/test/java/model/UsuarioModelTest.java
+++ src/test/java/model/UsuarioModelTest.java
@@ -4,22 +4,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import estudos.java.api.rest.model.UsuarioModel;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class UsuarioModelTest {
 
-    @Test
+    @RepeatedTest(10000)
     public void testGettersAndSetters() {
         UsuarioModel usuario = new UsuarioModel();
-        usuario.setCodigo(1);
-        usuario.setNome("Test User");
-        usuario.setLogin("testuser");
-        usuario.setSenha("password");
+        int codigo = (int) (Math.random() * 1000);
+        String nome = "Test User " + codigo;
+        String login = "testuser" + codigo;
+        String senha = "password" + codigo;
 
-        assertEquals(1, usuario.getCodigo().intValue());
-        assertEquals("Test User", usuario.getNome());
-        assertEquals("testuser", usuario.getLogin());
-        assertEquals("password", usuario.getSenha());
+        usuario.setCodigo(codigo);
+        usuario.setNome(nome);
+        usuario.setLogin(login);
+        usuario.setSenha(senha);
+
+        assertEquals(codigo, usuario.getCodigo().intValue());
+        assertEquals(nome, usuario.getNome());
+        assertEquals(login, usuario.getLogin());
+        assertEquals(senha, usuario.getSenha());
+        System.out.println("Teste numero: " + codigo);
     }
 
     @Test
