1009067345fb8ce5a9797ea7ac56008b45dbd537
Paulo Roberto <evans_sp@hotmail.com>
2023-04-09 18:17:53 -0300

    Corrindo ortografia!

--- src/main/java/estudos/java/api/rest/controller/UsuarioController.java
+++ src/main/java/estudos/java/api/rest/controller/UsuarioController.java
@@ -59,7 +59,7 @@ public class UsuarioController {
 
             repository.save(usuario);
 
-            return ResponseEntity.ok().body("Usuário cadastrado com sucesso");
+            return ResponseEntity.ok().body("Usuário cadastrado com sucesso!");
 
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
