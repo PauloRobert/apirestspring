07194bacf439b6b4e75efb67c2bcc7b36d42369b
Paulo Roberto <evans_sp@hotmail.com>
2023-04-09 18:39:26 -0300

    Incluindo validações ao salvar o usuário!

--- src/main/java/estudos/java/api/rest/controller/UsuarioController.java
+++ src/main/java/estudos/java/api/rest/controller/UsuarioController.java
@@ -10,9 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+
 
 
 @RestController
