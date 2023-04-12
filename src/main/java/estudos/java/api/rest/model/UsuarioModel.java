c52ff697a1818b251509ed7be2b9bd220f5bd4a1
Paulo Roberto <evans_sp@hotmail.com>
2023-04-12 08:01:55 -0300

    Aumentando o limite do campo senha, devido a conversão para Hash!

--- src/main/java/estudos/java/api/rest/model/UsuarioModel.java
+++ src/main/java/estudos/java/api/rest/model/UsuarioModel.java
@@ -35,12 +35,12 @@ public class UsuarioModel {
     @Column(nullable = false, length = 30)
     @NonNull
     @NotBlank(message = "O login é obrigatório")
-    @Size(max = 30, message = "O login não pode ter mais de 10 caracteres")
+    @Size(max = 30, message = "O login não pode ter mais de 30 caracteres")
     private  String login;
 
     // Anotação para informar que este atributo é uma coluna obrigatória com tamanho máximo de 10 caracteres
-    @Column(nullable = false, length = 30)
-    @Size(max = 30, message = "A senha não pode ter mais de 10 caracteres")
+    @Column(nullable = false, length = 200)
+    @Size(max = 200, message = "A senha não pode ter mais de 30 caracteres")
     private  String senha;
 
     public void setSenha(String senha) {
