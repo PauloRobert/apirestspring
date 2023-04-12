95ec5c3a6f71a5d97860ac50ea2136024e457db4
Paulo Roberto <evans_sp@hotmail.com>
2023-04-12 07:52:55 -0300

    Retirando import não uitilizado!

--- src/main/java/estudos/java/api/rest/model/UsuarioModel.java
+++ src/main/java/estudos/java/api/rest/model/UsuarioModel.java
@@ -10,7 +10,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import lombok.Data;
+
 
 // Anotação para criação automática de getters e setters, construtor padrão e outros métodos úteis do Lombok
 @NoArgsConstructor
