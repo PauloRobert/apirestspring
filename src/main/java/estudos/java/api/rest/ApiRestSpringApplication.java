73b4754699f787b6215de5d87eafab802bf1c8b6
Paulo Roberto <evans_sp@hotmail.com>
2023-04-23 16:54:01 -0300

    Refatcor

--- src/main/java/estudos/java/api/rest/ApiRestSpringApplication.java
+++ src/main/java/estudos/java/api/rest/ApiRestSpringApplication.java
@@ -1,8 +1,8 @@
 package estudos.java.api.rest;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class ApiRestSpringApplication {
 
